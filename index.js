d2b78a13bfb725552066d2f1bd2ba747fe6621d0
Bressain Dinkelman <dinkelburt@gmail.com>
2015-05-07 14:42:40 -0600

    Add initial example demo

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-import React from 'react'
+var React = require('react');
 
-class Component {
+class Comp extends React.Component {
+  constructor() {
+    super(arguments);
+    this.state = {};
+    this.state.punctuation = '.';
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      punctuation: React.findDOMNode(this.refs.thevalue).value
+    });
+  }
+
+  renderMessage() {
+    return this.props.message + this.state.punctuation;
+  }
+
   render() {
     return (
-      <h1>I'm ready to start me some Reactin'</h1>
+      <div>
+        <h1>{this.renderMessage()}</h1>
+        <input ref="thevalue" type="text" defaultValue="." />
+        <button onClick={this.handleClick}>Change Punctuation</button>
+      </div>
     )
   }
 }
 
-React.render(<Component />, document.getElementById('app'))
+var msg = "Hello, y'all";
+
+React.render(<Comp message={msg} />, document.getElementById('app'));
+
